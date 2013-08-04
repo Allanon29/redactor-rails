639be80a0b3fe3ff44c159e4cef62b4e33614e19
Allanon29 <allanon25@gmail.com>
2013-08-04 22:49:54 +0200

    Update helper.rb

--- lib/redactor-rails/helper.rb
+++ lib/redactor-rails/helper.rb
@@ -1,7 +1,7 @@
 module RedactorRails
   module Helpers
     # Setting Redactor Language
-    def redactor_lang(lang = 'en')
+    def redactor_lang(lang = 'hu')
       javascript_include_tag "redactor-rails/langs/#{lang}.js"
     end
   end
