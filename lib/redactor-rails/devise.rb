96e7f45d3f09064accf9daa30ab4c7a2838d7d82
Allanon29 <allanon25@gmail.com>
2013-08-04 22:48:47 +0200

    Update devise.rb

--- lib/redactor-rails/devise.rb
+++ lib/redactor-rails/devise.rb
@@ -1,11 +1,11 @@
 module RedactorRails
   module Devise
-    def redactor_authenticate_user!
-      authenticate_user!
+    def redactor_authenticate_member!
+      authenticate_member!
     end
 
-    def redactor_current_user
-      current_user
+    def redactor_current_member
+      current_member
     end
   end
 end
