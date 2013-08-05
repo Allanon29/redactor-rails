7722eb0e7eafe62a9e1cdfa35f65eae40d4cb152
Allanon29 <allanon25@gmail.com>
2013-08-05 15:48:50 +0200

    Update pictures_controller.rb

--- app/controller/redactor_rails/pictures_controller.rb
+++ app/controller/redactor_rails/pictures_controller.rb
@@ -1,9 +1,9 @@
 class RedactorRails::PicturesController < ApplicationController
-  before_filter :redactor_authenticate_member! if RedactorRails.picture_model.new.respond_to?(RedactorRails.devise_member)
+  before_filter :redactor_authenticate_member! if RedactorRails.picture_model.new.respond_to?(RedactorRails.devise_user)
 
   def index
     @pictures = RedactorRails.picture_model.where(
-        RedactorRails.picture_model.new.respond_to?(RedactorRails.devise_member) ? { RedactorRails.devise_member_key => redactor_current_member.id } : { })
+        RedactorRails.picture_model.new.respond_to?(RedactorRails.devise_user) ? { RedactorRails.devise_user_key => redactor_current_member.id } : { })
     render :json => @pictures.to_json
   end
 
@@ -12,8 +12,8 @@ class RedactorRails::PicturesController < ApplicationController
 
     file = params[:file]
     @picture.data = RedactorRails::Http.normalize_param(file, request)
-    if @picture.respond_to?(RedactorRails.devise_member)
-      @picture.send("#{RedactorRails.devise_member}=", redactor_current_member)
+    if @picture.respond_to?(RedactorRails.devise_user)
+      @picture.send("#{RedactorRails.devise_user}=", redactor_current_member)
       @picture.assetable = redactor_current_member
     end
 
