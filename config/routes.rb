249978457a0cd28a385bb7b1043542cc1345736b
Allanon29 <allanon25@gmail.com>
2013-08-21 07:11:36 +0200

    Added delete and destroy routes to pics

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 RedactorRails::Engine.routes.draw do
-  resources :pictures,  :only => [:index, :create]
+  resources :pictures,  :only => [:index, :create, :delete, :destroy]
   resources :documents, :only => [:index, :create]
 end
